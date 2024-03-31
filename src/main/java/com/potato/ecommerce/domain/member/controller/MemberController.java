aa36cafff385fbe5eaf5099c248235abbb7a6b50
yallo428 <yallo428@naver.com>
2024-03-31 23:33:53 +0900

    :sparkles: 유저 조회 기능

--- src/main/java/com/potato/ecommerce/domain/member/controller/MemberController.java
+++ src/main/java/com/potato/ecommerce/domain/member/controller/MemberController.java
@@ -1,13 +1,20 @@
 package com.potato.ecommerce.domain.member.controller;
 
-import com.potato.ecommerce.domain.member.dto.CreateMember;
+import com.potato.ecommerce.domain.member.dto.ResponseMember;
+import com.potato.ecommerce.domain.member.dto.SignUp;
+import com.potato.ecommerce.domain.member.dto.SignIn;
 import com.potato.ecommerce.domain.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +32,39 @@ public class MemberController {
     @Tag(name = "Member API")
     @Operation(summary = "회원가입", description = "회원가입입니다.")
     public ResponseEntity<Void> signUp(
-        @RequestBody @Validated CreateMember dto
-    ){
-        memberService.createMember(dto);
+        @RequestBody @Validated SignUp dto
+    ) {
+        memberService.signUp(dto);
         return ResponseEntity.status(204).build();
+    }
+
+    @PostMapping("/signin")
+    @Tag(name = "Member API")
+    @Operation(summary = "로그인", description = "로그인입니다.")
+    public ResponseEntity<Void> signIn(
+        @RequestBody @Validated SignIn dto
+    ) {
+        String token = memberService.signIn(dto);
+        ResponseCookie cookie = ResponseCookie
+            .from("Authorization", token)
+            .domain("localhost")
+            .path("/")
+            .httpOnly(true)
+            .maxAge(Duration.ofMinutes(30L))
+            .build();
+
+        return ResponseEntity.ok()
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
+            .build();
+    }
+
+    @GetMapping
+    @Tag(name = "Member API")
+    @Operation(summary = "정보 조회", description = "유저 조회입니다.")
+    public ResponseEntity<ResponseMember> getMember(
+        HttpServletRequest request
+    ) {
+        ResponseMember dto = memberService.getMember((String) request.getAttribute("subject"));
+        return ResponseEntity.ok(dto);
     }
 }
