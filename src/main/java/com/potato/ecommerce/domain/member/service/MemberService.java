86592df57233e56db49995d88df7a964c5860237
yallo428 <yallo428@naver.com>
2024-03-31 23:33:53 +0900

    :sparkles: 유저 조회 기능

--- src/main/java/com/potato/ecommerce/domain/member/service/MemberService.java
+++ src/main/java/com/potato/ecommerce/domain/member/service/MemberService.java
@@ -1,26 +1,67 @@
 package com.potato.ecommerce.domain.member.service;
 
-import com.potato.ecommerce.domain.member.dto.CreateMember;
+import com.potato.ecommerce.domain.member.dto.ResponseMember;
+import com.potato.ecommerce.domain.member.dto.SignUp;
+import com.potato.ecommerce.domain.member.dto.SignIn;
 import com.potato.ecommerce.domain.member.entity.MemberEntity;
+import com.potato.ecommerce.domain.member.entity.UserRoleEnum;
 import com.potato.ecommerce.domain.member.model.Member;
 import com.potato.ecommerce.domain.member.repository.MemberRepository;
+import com.potato.ecommerce.global.jwt.JwtUtil;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    public Member createMember(CreateMember dto) {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public void signUp(SignUp dto) {
         Member member = Member.builder()
             .email(dto.getEmail())
-            .password(dto.getPassword())
+            .password(passwordEncoder.encode(dto.getPassword()))
             .userName(dto.getUsername())
             .phone(dto.getPhone())
+            .role(UserRoleEnum.USER)
             .build();
 
         memberRepository.save(MemberEntity.fromModel(member));
-        return member;
     }
+
+    @Transactional
+    public String signIn(SignIn dto) {
+        Member member = findBy(dto.getEmail());
+
+        validatePassword(dto, member);
+
+        return jwtUtil.createToken(member.getEmail(), member.getRole());
+    }
+
+    public ResponseMember getMember(String subject) {
+        Member member = findBy(subject);
+
+        return member.createResponseDTO();
+    }
+
+    private Member findBy(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(
+            () -> new EntityNotFoundException("해당 유저가 존재하지 않습니다.")).toModel();
+    }
+
+    private void validatePassword(SignIn dto, Member member) {
+        if (member.isNotMatchPassword(passwordEncoder, dto.getPassword())) {
+            throw new ValidationException("패스워드가 일치하지 않습니다.");
+        }
+    }
+
 }
