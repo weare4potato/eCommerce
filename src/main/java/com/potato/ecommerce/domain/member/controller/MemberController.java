d2fba87ceef953d6534c00f314a49bb4ea23a9c1
yallo428 <yallo428@naver.com>
2024-03-31 16:53:23 +0900

    :sparkles: 회원가입

--- src/main/java/com/potato/ecommerce/domain/member/controller/MemberController.java
+++ src/main/java/com/potato/ecommerce/domain/member/controller/MemberController.java
@@ -1,10 +1,33 @@
 package com.potato.ecommerce.domain.member.controller;
 
+import com.potato.ecommerce.domain.member.dto.CreateMember;
+import com.potato.ecommerce.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Member API", description = "Member API 입니다.")
+@RequestMapping("/api/v1/users")
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    @Tag(name = "Member API")
+    @Operation(summary = "회원가입", description = "회원가입입니다.")
+    public ResponseEntity<Void> signUp(
+        @RequestBody @Validated CreateMember dto
+    ){
+        memberService.createMember(dto);
+        return ResponseEntity.status(204).build();
+    }
 }
--- src/main/java/com/potato/ecommerce/domain/member/service/MemberService.java
+++ src/main/java/com/potato/ecommerce/domain/member/service/MemberService.java
@@ -1,8 +1,26 @@
 package com.potato.ecommerce.domain.member.service;
 
+import com.potato.ecommerce.domain.member.dto.CreateMember;
+import com.potato.ecommerce.domain.member.entity.MemberEntity;
+import com.potato.ecommerce.domain.member.model.Member;
+import com.potato.ecommerce.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
+    private final MemberRepository memberRepository;
+    public Member createMember(CreateMember dto) {
+        Member member = Member.builder()
+            .email(dto.getEmail())
+            .password(dto.getPassword())
+            .userName(dto.getUsername())
+            .phone(dto.getPhone())
+            .build();
+
+        memberRepository.save(MemberEntity.fromModel(member));
+        return member;
+    }
 }
