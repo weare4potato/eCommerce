f3ed471bd5d87c37dd624ccb25993f616499f1ae
tangpoo <lhinav@naver.com>
2024-03-31 15:35:24 +0900

    :sparkles: 사업자 번호 발급 구현

---
+++ src/main/java/com/potato/ecommerce/domain/revenue/dto/ResponseRevenue.java
@@ -0,0 +1,10 @@
+package com.potato.ecommerce.domain.revenue.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseRevenue {
+    private String number;
+}
---
+++ src/main/java/com/potato/ecommerce/domain/revenue/model/Revenue.java
@@ -0,0 +1,19 @@
+package com.potato.ecommerce.domain.revenue.model;
+
+import lombok.Getter;
+
+@Getter
+public class Revenue {
+
+    private Long id;
+    private String number;
+
+    public Revenue(Long number) {
+        this.number = String.format("%09d", number);
+    }
+
+    public Revenue(Long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+}
