c246c6e2f8330787edd140fdb285dc63910a9944
yallo428 <yallo428@naver.com>
2024-04-02 01:55:22 +0900

    :recycle: Long -> String

--- src/main/java/com/potato/ecommerce/domain/member/repository/MemberJpaRepository.java
+++ src/main/java/com/potato/ecommerce/domain/member/repository/MemberJpaRepository.java
@@ -4,7 +4,7 @@ import com.potato.ecommerce.domain.member.entity.MemberEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberJpaRepository extends JpaRepository<MemberEntity, String> {
+public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
 
     Optional<MemberEntity> findByEmail(String email);
 }
