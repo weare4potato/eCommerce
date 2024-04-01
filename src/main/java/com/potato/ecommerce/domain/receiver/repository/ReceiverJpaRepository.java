294df84d4f8f2fb4beedf22c2e82491606b26ae5
yallo428 <yallo428@naver.com>
2024-04-01 19:40:40 +0900

    :recycle: rename

--- src/main/java/com/potato/ecommerce/domain/receiver/repository/ReceiverJpaRepository.java
+++ src/main/java/com/potato/ecommerce/domain/receiver/repository/ReceiverJpaRepository.java
@@ -3,6 +3,6 @@ package com.potato.ecommerce.domain.receiver.repository;
 import com.potato.ecommerce.domain.receiver.entity.ReceiverEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReceiverRepository extends JpaRepository<ReceiverEntity, Long> {
+public interface ReceiverJpaRepository extends JpaRepository<ReceiverEntity, Long> {
 
 }
