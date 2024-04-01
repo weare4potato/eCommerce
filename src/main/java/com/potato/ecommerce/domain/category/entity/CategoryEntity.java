a9692b410a251897a8a03fb32c5c5e6764efec60
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-01 12:02:21 +0900

    :bug: Table name 복수형으로 수정, Repository 어노테이션 생략

--- src/main/java/com/potato/ecommerce/domain/category/entity/CategoryEntity.java
+++ src/main/java/com/potato/ecommerce/domain/category/entity/CategoryEntity.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "categorys")
+@Table(name = "Categories")
 public class CategoryEntity {
 
     @Id
