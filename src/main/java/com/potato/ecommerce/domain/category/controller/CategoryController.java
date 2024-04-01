c5a28c2ff119708f27743807e8e35fedc5cf0168
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-01 10:49:24 +0900

    :sparkles: 카테고리 대분류 조회

--- src/main/java/com/potato/ecommerce/domain/category/controller/CategoryController.java
+++ src/main/java/com/potato/ecommerce/domain/category/controller/CategoryController.java
@@ -1,8 +1,24 @@
 package com.potato.ecommerce.domain.category.controller;
 
+import com.potato.ecommerce.domain.category.service.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/oneDepth")
+    public ResponseEntity<List<String>> getAllOneDepthCategories() {
+        List<String> oneDepthCategories = categoryService.getAllOneDepthCategories();
+        return ResponseEntity.ok(oneDepthCategories);
+    }
 
 }
--- src/main/java/com/potato/ecommerce/domain/category/repository/CategoryRepository.java
+++ src/main/java/com/potato/ecommerce/domain/category/repository/CategoryRepository.java
@@ -1,8 +1,16 @@
 package com.potato.ecommerce.domain.category.repository;
 
 import com.potato.ecommerce.domain.category.entity.CategoryEntity;
+import com.potato.ecommerce.domain.category.entity.CategoryType;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+    @Query("SELECT DISTINCT c.oneDepth FROM CategoryEntity c")
+    List<String> findDistinctOneDepth();
 
 }
