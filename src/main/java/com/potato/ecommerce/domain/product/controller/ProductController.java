d9314f2e0d18a24e34d0e4e529acea3136925d3a
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-02 17:00:27 +0900

    :sparkles: product 상품 등록 기능 구현

--- src/main/java/com/potato/ecommerce/domain/product/controller/ProductController.java
+++ src/main/java/com/potato/ecommerce/domain/product/controller/ProductController.java
@@ -10,6 +10,7 @@ import com.potato.ecommerce.domain.store.entity.StoreEntity;
 import com.potato.ecommerce.domain.store.repository.StoreRepository;
 import com.potato.ecommerce.global.jwt.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -28,18 +29,16 @@ public class ProductController {
     private final JwtUtil jwtUtil;
 
     @PostMapping
-    public ResponseEntity<ProductResponse> createProduct(
-        @RequestBody ProductRequest requestDto,
-        HttpServletRequest request) {
-
+    public ResponseEntity<?> createProduct(@Valid @RequestBody ProductRequest requestDto, HttpServletRequest request) {
         String token = jwtUtil.getTokenFromRequest(request);
         if (!jwtUtil.validateToken(token)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         String sellerBusinessNumber = jwtUtil.getUserInfoFromToken(token).getSubject();
-        ProductResponse response = productService.createProduct(sellerBusinessNumber, requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        productService.createProduct(sellerBusinessNumber, requestDto);
+        // 리스폰스 본문 없이 HTTP 상태 코드 201 반환
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
--- src/main/java/com/potato/ecommerce/domain/product/model/Product.java
+++ src/main/java/com/potato/ecommerce/domain/product/model/Product.java
@@ -1,12 +1,52 @@
 package com.potato.ecommerce.domain.product.model;
 
+import com.potato.ecommerce.domain.category.entity.CategoryEntity;
+import com.potato.ecommerce.domain.product.entity.ProductEntity;
+import com.potato.ecommerce.domain.store.entity.StoreEntity;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class Product {
 
+    private Long id;
+    private StoreEntity store;
+    private CategoryEntity category;
+    private String name;
+    private String description;
+    private Integer price;
+    private Integer stock;
+    private Boolean isDelete;
+    private LocalDateTime createdAt;
+
+    public ProductEntity toEntity() {
+        return ProductEntity.builder()
+            .id(id)
+            .store(store)
+            .category(category)
+            .name(name)
+            .description(description)
+            .price(price)
+            .stock(stock)
+            .isDelete(isDelete)
+            .build();
+    }
+
+    @Builder
+    public Product(Long id, StoreEntity store, CategoryEntity category, String name,
+        String description,
+        Integer price, Integer stock, Boolean isDelete, LocalDateTime createdAt) {
+        this.id = id;
+        this.store = store;
+        this.category = category;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.isDelete = isDelete;
+        this.createdAt = createdAt;
+    }
 }
