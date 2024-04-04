9f7087afd1629bb6e9bb7913297c1643f6fbb038
oyeonjoo <oyj0842@naver.com>
2024-04-04 16:59:03 +0900

    :fire: favorite package 제거

--- src/main/java/com/potato/ecommerce/domain/favorite/repository/FavoriteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.potato.ecommerce.domain.favorite.repository;
-
-import com.potato.ecommerce.domain.favorite.entity.FavoriteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Long> {
-
-}
--- src/main/java/com/potato/ecommerce/domain/favorite/service/FavoriteService.java
+++
@@ -1,8 +0,0 @@
-package com.potato.ecommerce.domain.favorite.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FavoriteService {
-
-}
