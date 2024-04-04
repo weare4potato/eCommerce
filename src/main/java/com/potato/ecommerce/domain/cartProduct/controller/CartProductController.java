0162413f10a81975c8f8dd8b22af54aad18382fd
oyeonjoo <oyj0842@naver.com>
2024-04-04 15:45:55 +0900

    :fire: CartProduct 패키지 삭제

--- src/main/java/com/potato/ecommerce/domain/cartProduct/controller/CartProductController.java
+++
@@ -1,8 +0,0 @@
-package com.potato.ecommerce.domain.cartProduct.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CartProductController {
-
-}
