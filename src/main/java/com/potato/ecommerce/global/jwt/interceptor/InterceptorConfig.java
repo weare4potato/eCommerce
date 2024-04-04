43e14cfae671a27d5d00b867c0d13a2edcb157cf
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-04 15:42:34 +0900

    :recycle: is_deleted 칼럼명 수정, 조회 API URL 수정

--- src/main/java/com/potato/ecommerce/global/jwt/interceptor/InterceptorConfig.java
+++ src/main/java/com/potato/ecommerce/global/jwt/interceptor/InterceptorConfig.java
@@ -24,9 +24,11 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 "/api/v1/revenue",
                 "/api/v1/orders/**",
                 "/api/v1/users/signup/confirm",
-                "/api/v1/products/**",
+                "/api/v1/products/all",
+                "/api/v1/products/details/*",
                 "/api/v1/shops/*/shop-products"
             );
+
 
     }
 }
