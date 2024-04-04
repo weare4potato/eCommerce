4adfed234bcc7d9e76b5c3db55638546bdbfd7cb
oyeonjoo <oyj0842@naver.com>
2024-04-04 16:54:51 +0900

    :recycle: @Getter 추가

--- src/main/java/com/potato/ecommerce/domain/member/entity/MemberEntity.java
+++ src/main/java/com/potato/ecommerce/domain/member/entity/MemberEntity.java
@@ -16,11 +16,13 @@ import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
