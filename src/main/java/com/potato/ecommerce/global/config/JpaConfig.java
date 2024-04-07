7d19e3f068d6e9085136ed12f544afcde9e51c14
tangpoo <lhinav@naver.com>
2024-04-07 15:12:44 +0900

    :recycle: 파일 위치 수정

--- src/main/java/com/potato/ecommerce/global/config/JpaConfig.java
+++ src/main/java/com/potato/ecommerce/global/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.potato.ecommerce.global.config.redis;
+package com.potato.ecommerce.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
