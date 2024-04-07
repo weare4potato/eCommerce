9c6792d1e8a7bb9c136b2c5a3de0960af607a40a
tangpoo <lhinav@naver.com>
2024-04-07 17:23:46 +0900

    :white_check_mark: StoreService, Store 테스트 코드 작성

--- src/main/java/com/potato/ecommerce/domain/store/dto/DeleteStoreRequest.java
+++ src/main/java/com/potato/ecommerce/domain/store/dto/DeleteStoreRequest.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class DeleteStoreRequest {
 
     @NotBlank
--- src/main/java/com/potato/ecommerce/domain/store/dto/UpdateStoreRequest.java
+++ src/main/java/com/potato/ecommerce/domain/store/dto/UpdateStoreRequest.java
@@ -1,9 +1,11 @@
 package com.potato.ecommerce.domain.store.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UpdateStoreRequest {
 
     @NotBlank
