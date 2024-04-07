00352da31c392e081f412c44384adde3e40c14d0
oyeonjoo <oyj0842@naver.com>
2024-04-08 04:17:36 +0900

    :recycle: 주문 목록 조회시 페이징 기능 추가

---
+++ src/main/java/com/potato/ecommerce/domain/order/repository/OrderQueryRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.potato.ecommerce.domain.order.repository;
+
+import static com.potato.ecommerce.domain.order.entity.QOrderEntity.orderEntity;
+import static com.querydsl.core.types.Projections.fields;
+
+import com.potato.ecommerce.domain.order.dto.OrderList;
+import com.potato.ecommerce.domain.order.entity.QOrderEntity;
+import com.potato.ecommerce.global.util.RestPage;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderQueryRepositoryImpl implements OrderQueryRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public RestPage<OrderList> getOrders(String subject, int page, int size) {
+        QOrderEntity order = orderEntity;
+
+        List<OrderList> orders =
+            jpaQueryFactory.select(fields(OrderList.class,
+                    order.id,
+                    order.status,
+                    order.orderedAt,
+                    order.orderNum))
+                .from(order)
+                .where(order.member.email.eq(subject))
+                .orderBy(order.orderedAt.desc())
+                .offset(page)
+                .limit(size)
+                .fetch();
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        return new RestPage<>(new PageImpl<>(orders, pageable, orders.size()));
+    }
+}
