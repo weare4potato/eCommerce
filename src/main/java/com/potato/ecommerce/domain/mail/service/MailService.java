cc0d718ae63f49a9b2442b2144b388f2fbaf383a
yallo428 <yallo428@naver.com>
2024-04-08 21:49:56 +0900

    :recycle: add createMessage

--- src/main/java/com/potato/ecommerce/domain/mail/service/MailService.java
+++ src/main/java/com/potato/ecommerce/domain/mail/service/MailService.java
@@ -1,7 +1,6 @@
 package com.potato.ecommerce.domain.mail.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,16 +12,22 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
 
-    public void sendMail(String email) throws MailException {
-        SimpleMailMessage message = new SimpleMailMessage();
+    public String sendMail(String email) throws MailException {
+        javaMailSender.send(createMessage(email));
 
+        return "이메일 인증을 해주세요";
+    }
+
+
+    private SimpleMailMessage createMessage(String email){
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("회원가입 이메일 인증");
         message.setText("이메일 인증\n"
             + "아래 링크를 클릭해주시면 이메일 인증이 완료됩니다.\n"
             + "http://localhost:8080/api/v1/users/signup/confirm?email="
-            + email);
-
-        javaMailSender.send(message);
+            + email
+        );
+        return message;
     }
 }
