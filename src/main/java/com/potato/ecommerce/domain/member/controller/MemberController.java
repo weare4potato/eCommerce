e74a99a538138f5f0363e60288b848a14cbf4609
yallo428 <yallo428@naver.com>
2024-04-08 21:50:43 +0900

    :recycle: 반환 타입 통일

--- src/main/java/com/potato/ecommerce/domain/member/controller/MemberController.java
+++ src/main/java/com/potato/ecommerce/domain/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.potato.ecommerce.domain.member.dto.SignInDto;
 import com.potato.ecommerce.domain.member.dto.UpdateMemberDto;
 import com.potato.ecommerce.domain.member.dto.UpdatePasswordDto;
 import com.potato.ecommerce.domain.member.service.MemberService;
+import com.potato.ecommerce.global.jwt.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,7 @@ import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -32,101 +34,97 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class MemberController {
 
-    /*
-    TODO : 로그아웃, 회원 탈퇴
-     */
-
     private final MemberService memberService;
     private final MailService mailService;
 
     @PostMapping("/signup")
-    @Tag(name = "Member API")
     @Operation(summary = "회원가입")
     public ResponseEntity<String> signUp(
         @RequestBody @Validated SignUpDto dto
     ) {
         memberService.signUp(dto);
-        mailService.sendMail(dto.getEmail());
-        return ResponseEntity.status(201).body("이메일 인증을 해주세요");
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(mailService.sendMail(dto.getEmail()));
     }
 
     @PostMapping("/signin")
-    @Tag(name = "Member API")
     @Operation(summary = "로그인")
     public ResponseEntity<Void> signIn(
         @RequestBody @Validated SignInDto dto
     ) {
         String token = memberService.signIn(dto);
         ResponseCookie cookie = ResponseCookie
-            .from("Authorization", token)
+            .from(JwtUtil.AUTHORIZATION_HEADER, token)
             .domain("localhost")
             .path("/")
             .httpOnly(true)
             .maxAge(Duration.ofMinutes(30L))
             .build();
 
-        return ResponseEntity.ok()
+        return ResponseEntity
+            .status(HttpStatus.OK)
             .header(HttpHeaders.SET_COOKIE, cookie.toString())
             .build();
     }
 
     @GetMapping
-    @Tag(name = "Member API")
     @Operation(summary = "정보 조회")
     public ResponseEntity<ResponseMember> getMember(
         HttpServletRequest request
     ) {
-        String subject = getSubject(request);
-        ResponseMember dto = memberService.getMember(subject);
-        return ResponseEntity.ok(dto);
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(memberService.getMember(getSubject(request)));
     }
 
     @PostMapping
-    @Tag(name = "Member API")
     @Operation(summary = "비밀번호 확인")
     public ResponseEntity<Void> passwordCheck(
         @RequestBody String password,
         HttpServletRequest request
     ) {
         memberService.passwordCheck(getSubject(request), password);
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .build();
     }
 
-
     @PutMapping
-    @Tag(name = "Member API")
     @Operation(summary = "정보 수정")
     public ResponseEntity<ResponseMember> updateMember(
         @RequestBody @Validated UpdateMemberDto dto,
         HttpServletRequest request
     ) {
-        return ResponseEntity.ok(
-            memberService.updateMember(
-                dto,
-                getSubject(request))
-        );
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(memberService.updateMember(dto, getSubject(request)));
     }
 
     @PutMapping("/password")
-    @Tag(name = "Member API")
     @Operation(summary = "비밀번호 수정")
     public ResponseEntity<String> updatePassword(
         @RequestBody @Validated UpdatePasswordDto dto,
         HttpServletRequest request
     ) {
         memberService.updatePassword(dto, getSubject(request));
-        return ResponseEntity.ok("비밀번호 변경이 완료되었습니다.");
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body("비밀번호 변경이 완료되었습니다.");
+    }
+
+    @GetMapping("/signup/confirm")
+    public ResponseEntity<String> updateAuth(
+        @RequestParam("email") String email
+    ) {
+        memberService.confirmMember(email);
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body("인증이 완료 되었습니다.");
     }
 
     private String getSubject(HttpServletRequest request) {
         return (String) request.getAttribute("subject");
     }
 
-    @GetMapping("/signup/confirm")
-    public ResponseEntity<String> updateAuth(
-        @RequestParam("email") String email
-    ){
-        memberService.confirmMember(email);
-        return ResponseEntity.status(201).body("인증이 완료 되었습니다.");
-    }
 }
