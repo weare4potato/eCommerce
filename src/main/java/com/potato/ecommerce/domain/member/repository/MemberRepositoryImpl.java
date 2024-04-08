4f70af068ae0134a77a133ef5936953ef2609ee7
yallo428 <yallo428@naver.com>
2024-04-08 22:41:28 +0900

    :recycle: 패키지 정리

--- src/main/java/com/potato/ecommerce/domain/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/potato/ecommerce/domain/member/repository/MemberRepositoryImpl.java
@@ -4,7 +4,6 @@ import com.potato.ecommerce.domain.member.entity.MemberEntity;
 import com.potato.ecommerce.domain.member.model.Member;
 import com.potato.ecommerce.global.exception.ExceptionMessage;
 import jakarta.persistence.EntityNotFoundException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -16,18 +15,18 @@ public class MemberRepositoryImpl implements MemberRepository{
 
     @Override
     public Member findMemberBy(String email) {
-        return memberJpaRepository.findByEmail(email).orElseThrow(
+        return Member.fromEntity(memberJpaRepository.findByEmail(email).orElseThrow(
             () -> new EntityNotFoundException(ExceptionMessage.MEMBER_NOT_FOUND.toString())
-        ).toModel();
+        ));
     }
 
     @Override
     public void save(Member member) {
-        memberJpaRepository.save(MemberEntity.fromModel(member));
+        memberJpaRepository.save(member.toEntity());
     }
 
     @Override
     public void update(Member member) {
-        memberJpaRepository.save(MemberEntity.fromModel(member));
+        memberJpaRepository.save(member.toEntity());
     }
 }
