f9298afe7421d135f8d3dd4fe94b0a7aedd8fa50
oyeonjoo <oyj0842@naver.com>
2024-04-09 18:00:24 +0900

    :recycle: exception message 추가

--- src/main/java/com/potato/ecommerce/global/exception/ExceptionMessage.java
+++ src/main/java/com/potato/ecommerce/global/exception/ExceptionMessage.java
@@ -10,10 +10,11 @@ public enum ExceptionMessage {
     BUSINESS_NUMBER_NOT_MATCH("사업자 등록 번호가 일치하지 않습니다."),
     DUPLICATE_BUSINESS_NUMBER("이미 사용된 사업자 번호입니다."),
     BUSINESS_NUMBER_NOT_FOUNT("발급된 사업자 번호가 아닙니다."),
-    RECEIVER_NOT_MATCH("해당 주소지를 갖고 있지 않습니다."),
+    RECEIVER_NOT_MATCH("주소지가 일치하지 않습니다."),
     ORDER_NOT_FOUND("해당 주문을 찾지 못했습니다."),
     PRODUCT_NOT_FOUND("해당 상품을 찾지 못했습니다."),
-    STORE_NOT_FOUNT("상점을 찾을 수 없습니다.");
+    STORE_NOT_FOUND("해당 상점을 찾지 못했습니다."),
+    CART_NOT_FOUND("해당 장바구니를 찾지 못했습니다.");
 
     private final String message;
 
