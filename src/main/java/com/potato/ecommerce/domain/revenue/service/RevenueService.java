5130b2f58d2a7f78db2d3cd180330127b2e7d21b
tangpoo <lhinav@naver.com>
2024-04-10 21:19:48 +0900

    :recycle: 사업자 번호 Tread-safe 한 AtomicLong 으로 변경

--- src/main/java/com/potato/ecommerce/domain/revenue/service/RevenueService.java
+++ src/main/java/com/potato/ecommerce/domain/revenue/service/RevenueService.java
@@ -3,6 +3,8 @@ package com.potato.ecommerce.domain.revenue.service;
 import com.potato.ecommerce.domain.revenue.dto.RevenueResponse;
 import com.potato.ecommerce.domain.revenue.entity.RevenueEntity;
 import com.potato.ecommerce.domain.revenue.repository.RevenueRepository;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,10 @@ import org.springframework.stereotype.Service;
 public class RevenueService {
 
     private final RevenueRepository revenueRepository;
-    private Long number = 1L;
+    private AtomicLong seq = new AtomicLong(1);
 
     public RevenueResponse getBusinessNumber() {
-        RevenueEntity revenueEntity = new RevenueEntity(number++);
+        RevenueEntity revenueEntity = new RevenueEntity(seq);
 
         RevenueEntity revenue = revenueRepository.save(revenueEntity);
 
