9be9e87c1860145656293652966c574b265f9dae
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-11 11:53:53 +0900

    :recycle: 생성자 수정

--- src/main/java/com/potato/ecommerce/domain/product/dto/ProductSimpleResponse.java
+++ src/main/java/com/potato/ecommerce/domain/product/dto/ProductSimpleResponse.java
@@ -1,5 +1,6 @@
 package com.potato.ecommerce.domain.product.dto;
 
+import com.potato.ecommerce.domain.product.entity.ProductEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,5 +10,9 @@ public class ProductSimpleResponse {
 
     String name;
     Integer price;
+
+    public static ProductSimpleResponse of(ProductEntity productEntity) {
+        return new ProductSimpleResponse(productEntity.getName(), productEntity.getPrice());
+    }
 
 }
