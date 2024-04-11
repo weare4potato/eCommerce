1f9b7fe6ee9339ceb7c026caf22117c06c87bcea
yallo428 <yallo428@naver.com>
2024-04-12 01:15:36 +0900

    :bug:(Order) Dto 내부 Entity -> Dto로 변환

--- src/main/java/com/potato/ecommerce/domain/order/controller/dto/response/OrderInfoWithHistoryResponseDTO.java
+++ src/main/java/com/potato/ecommerce/domain/order/controller/dto/response/OrderInfoWithHistoryResponseDTO.java
@@ -1,10 +1,12 @@
 package com.potato.ecommerce.domain.order.controller.dto.response;
 
+import com.potato.ecommerce.domain.member.dto.ResponseMember;
 import com.potato.ecommerce.domain.member.entity.MemberEntity;
 import com.potato.ecommerce.domain.order.dto.HistoryInfo;
 import com.potato.ecommerce.domain.order.dto.OrderInfoWithHistory;
 import com.potato.ecommerce.domain.order.entity.OrderStatus;
 import com.potato.ecommerce.domain.payment.vo.PaymentType;
+import com.potato.ecommerce.domain.receiver.dto.ReceiverForm;
 import com.potato.ecommerce.domain.receiver.entity.ReceiverEntity;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,16 +14,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Slf4j
 public class OrderInfoWithHistoryResponseDTO {
 
     private Long id;
-    private MemberEntity member;
-    private ReceiverEntity receiver;
+    private ResponseMember member;
+    private ReceiverForm receiver;
     private PaymentType paymentType;
     private OrderStatus status;
     private String orderNum;
