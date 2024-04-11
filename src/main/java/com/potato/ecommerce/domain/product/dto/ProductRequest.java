45c300b93f94b819b983f872cd65d99c1a2ba28a
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-11 11:28:44 +0900

    :recycle: Request 패턴 적용

--- src/main/java/com/potato/ecommerce/domain/product/dto/ProductRequest.java
+++ src/main/java/com/potato/ecommerce/domain/product/dto/ProductRequest.java
@@ -1,22 +1,22 @@
 package com.potato.ecommerce.domain.product.dto;
 
-import com.potato.ecommerce.domain.category.entity.CategoryEntity;
-import com.potato.ecommerce.domain.product.entity.ProductEntity;
-import com.potato.ecommerce.domain.store.entity.StoreEntity;
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class ProductRequest {
 
     private Long categoryId;
+
+    @Size(max = 150)
     private String name;
+
     private String description;
+
     private Integer price;
+
     private Integer stock;
 
 }
