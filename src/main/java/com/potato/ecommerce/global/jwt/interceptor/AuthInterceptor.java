fd7dc51d38733d100cebc9cc41a52d18b83dfb87
yallo428 <yallo428@naver.com>
2024-04-12 14:35:44 +0900

    :fire: default method 삭제

--- src/main/java/com/potato/ecommerce/global/jwt/interceptor/AuthInterceptor.java
+++ src/main/java/com/potato/ecommerce/global/jwt/interceptor/AuthInterceptor.java
@@ -33,15 +33,4 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-        ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
-        Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
-    }
 }
