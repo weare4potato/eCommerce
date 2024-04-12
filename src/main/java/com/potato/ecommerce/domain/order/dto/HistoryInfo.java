b9d6479e66a1ea1e87c176808a14e161323eaa2d
yallo428 <yallo428@naver.com>
2024-04-12 21:39:31 +0900

    :bug: 단일 조회 오류 해결

--- src/main/java/com/potato/ecommerce/domain/order/dto/HistoryInfo.java
+++ src/main/java/com/potato/ecommerce/domain/order/dto/HistoryInfo.java
@@ -1,27 +1,29 @@
 package com.potato.ecommerce.domain.order.dto;
 
 import com.potato.ecommerce.domain.order.entity.HistoryEntity;
-import com.potato.ecommerce.domain.product.entity.ProductEntity;
+import com.potato.ecommerce.domain.product.dto.ProductSimpleResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class HistoryInfo {
 
     private Long id;
-    private ProductEntity product;
+    private ProductSimpleResponse product;
     private Integer quantity;
     private Long orderPrice;
 
-    public static HistoryInfo fromEntity(HistoryEntity historyEntity) {
+    public static HistoryInfo fromEntity(HistoryEntity historyEntity, ProductSimpleResponse product) {
         return HistoryInfo.builder()
             .id(historyEntity.getId())
-            .product(historyEntity.getProduct())
+            .product(product)
             .quantity(historyEntity.getQuantity())
             .orderPrice(historyEntity.getOrderPrice())
             .build();
