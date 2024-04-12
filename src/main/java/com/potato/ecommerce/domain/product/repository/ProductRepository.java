d828f714b941dd95045516d79e097d69e95b7a79
haeun0405 <124241345+haeun0405@users.noreply.github.com>
2024-04-12 10:41:55 +0900

    :recycle: category 리팩토링 후 Entity 변경

--- src/main/java/com/potato/ecommerce/domain/product/repository/ProductRepository.java
+++ src/main/java/com/potato/ecommerce/domain/product/repository/ProductRepository.java
@@ -9,11 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findByStoreId(Long storeId);
 
-    List<ProductEntity> findByCategoryId(Long categoryId);
+    List<ProductEntity> findByProductCategory_Id(Long productCategoryId);
 
     Page<ProductEntity> findAll(Pageable pageable);
 
     Page<ProductEntity> findByStoreId(Long storeId, Pageable pageable);
-    Page<ProductEntity> findByCategoryId(Long categoryId, Pageable pageable);
+    Page<ProductEntity> findByProductCategory_Id(Long productCategoryId, Pageable pageable);
 
 }
