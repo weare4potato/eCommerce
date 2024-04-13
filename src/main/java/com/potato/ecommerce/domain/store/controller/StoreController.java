dfdf6d96cd2d745789cc2b2c38407852eb55a286
tangpoo <lhinav@naver.com>
2024-04-13 23:10:26 +0900

    :recycle: getProducts 사용 안하는 shopsId 제거

--- src/main/java/com/potato/ecommerce/domain/store/controller/StoreController.java
+++ src/main/java/com/potato/ecommerce/domain/store/controller/StoreController.java
@@ -109,7 +109,7 @@ public class StoreController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(STORE_DELETE_SUCCESS);
     }
 
-    @GetMapping("/{shopsId}/products")
+    @GetMapping("/products")
     @Operation(summary = "등록한 상품 조회")
     public ResponseEntity<RestPage<ProductListResponse>> getProducts(
         HttpServletRequest httpServletRequest,
