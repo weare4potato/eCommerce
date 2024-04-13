35560b70a52a4c530117b50077265d39b819235e
tangpoo <lhinav@naver.com>
2024-04-13 18:18:38 +0900

    :sparkles: projection error 해결 -> ProductListResponse 기본 생성자 추가

--- src/main/java/com/potato/ecommerce/domain/product/dto/ProductListResponse.java
+++ src/main/java/com/potato/ecommerce/domain/product/dto/ProductListResponse.java
@@ -2,13 +2,15 @@ package com.potato.ecommerce.domain.product.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductListResponse {
 
     private String name;
-    private Long categoryId;
-    private Integer price;
+    private Long productCategory;
+    private Long price;
     private Integer stock;
 }
