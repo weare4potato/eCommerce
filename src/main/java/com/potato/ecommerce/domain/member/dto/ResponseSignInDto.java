ab0d0c70068c2d0a083e9c6e90c4bd6b1fe3b202
yallo428 <yallo428@naver.com>
2024-04-14 20:59:18 +0900

    :recycle:Getter 추가

--- src/main/java/com/potato/ecommerce/domain/member/dto/ResponseSignInDto.java
+++ src/main/java/com/potato/ecommerce/domain/member/dto/ResponseSignInDto.java
@@ -1,7 +1,11 @@
 package com.potato.ecommerce.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 @Builder
 public class ResponseSignInDto {
 
