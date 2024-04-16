f7cc6a2b3edd498bd722b4526cb019c1d492d06b
yallo428 <yallo428@naver.com>
2024-04-16 22:49:59 +0900

    :recycle: validation 메시지 추가 및 validExceptionHandler 반환 타입 변경

--- src/main/java/com/potato/ecommerce/domain/member/dto/SignUpDto.java
+++ src/main/java/com/potato/ecommerce/domain/member/dto/SignUpDto.java
@@ -18,15 +18,15 @@ public class SignUpDto {
     private String email;
 
     @NotBlank
-    @Size(min = 2, max = 12)
+    @Size(min = 2, max = 12, message = "최소 2글자 최대 12글자로 작성해주세요.")
     private String username;
 
     @NotBlank
-    @Size(min = 8, max = 15)
-    @Pattern(regexp = "^[a-zA-Z0-9.!@#$]*$")
+    @Size(min = 8, max = 15,  message = "최소 8글자 최대 15글자로 작성해주세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9.!@#$]*$", message = "소문자, 대문자, 특수문자가 1개씩 포함되어야 합니다.")
     private String password;
 
     @NotBlank
-    @Pattern(regexp = "^01(0)-([0-9]{4})-([0-9]{4})$")
+    @Pattern(regexp = "^01(0)-([0-9]{4})-([0-9]{4})$", message = "전화번호 양식에 맞게 작성해주세요.")
     private String phone;
 }
