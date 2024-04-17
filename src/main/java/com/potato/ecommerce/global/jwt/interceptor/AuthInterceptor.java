ee14f28d636959c67cd3ead2e701689fd4f3380a
yallo428 <yallo428@naver.com>
2024-04-18 02:08:15 +0900

    :recycle: preflightRequest 해결

--- src/main/java/com/potato/ecommerce/global/jwt/interceptor/AuthInterceptor.java
+++ src/main/java/com/potato/ecommerce/global/jwt/interceptor/AuthInterceptor.java
@@ -3,8 +3,10 @@ package com.potato.ecommerce.global.jwt.interceptor;
 import com.potato.ecommerce.global.jwt.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -15,12 +17,15 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class AuthInterceptor implements HandlerInterceptor {
 
     private final JwtUtil jwtUtil;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
+        if (isPreflightRequest(request)) {
+            return true;
+        }
         String token = jwtUtil.getTokenFromHeader(request);
-
-        if(ObjectUtils.isEmpty(token)){
+        if (ObjectUtils.isEmpty(token)) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return false;
         }
@@ -32,4 +37,27 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     }
 
+    private boolean isPreflightRequest(HttpServletRequest request) {
+        return isOptions(request) && hasHeaders(request) && hasMethod(request) && hasOrigin(
+            request);
+    }
+
+    private boolean isOptions(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.toString());
+    }
+
+    private boolean hasHeaders(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Headers"));
+    }
+
+    private boolean hasMethod(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Access-Control-Request-Method"));
+    }
+
+    private boolean hasOrigin(HttpServletRequest request) {
+        return Objects.nonNull(request.getHeader("Origin"));
+    }
 }
+
+
+
