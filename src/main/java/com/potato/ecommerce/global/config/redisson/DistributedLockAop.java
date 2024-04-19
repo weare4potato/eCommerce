fed8c59195332cf2b01d4088f8eb51727734adaf
oyeonjoo <oyj0842@naver.com>
2024-04-19 17:17:32 +0900

    :recycle: lock 획득 재시도 및 타임아웃 변경, 획득 실패시 예외처리 로직 추가

--- src/main/java/com/potato/ecommerce/global/config/redisson/DistributedLockAop.java
+++ src/main/java/com/potato/ecommerce/global/config/redisson/DistributedLockAop.java
@@ -18,9 +18,17 @@ import org.springframework.stereotype.Component;
 public class DistributedLockAop {
 
     private static final String REDISSON_LOCK_PREFIX = "LOCK: ";
+    private static final long RETRY_DELAY = 3L; // 재시도 사이의 대기 시간
+    private static final long MAX_RETRY_COUNT = 5L; // 최대 재시도 횟수
+
 
     private final RedissonClient redissonClient;
     private final AopForTransaction aopForTransaction;
+
+    // Redis 데이터 초기화
+    public void flushAll() {
+        redissonClient.getKeys().flushall();
+    }
 
     @Around("@annotation(com.potato.ecommerce.global.config.redisson.DistributedLock)")
     public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
@@ -35,25 +43,52 @@ public class DistributedLockAop {
 
         RLock rLock = redissonClient.getLock(key);
 
-        try {
-            System.out.println("key: " + key);
-            boolean available = rLock.tryLock(distributedLock.waitTime(),
-                distributedLock.leaseTime(), distributedLock.timeUnit());
+//        try {
+//            System.out.println("key: " + key);
+//            boolean available = rLock.tryLock(distributedLock.waitTime(),
+//                distributedLock.leaseTime(), distributedLock.timeUnit());
+//
+//            if (!available) {
+//                throw new IllegalStateException("DistributedLock lock failed");
+//            }
+//            return aopForTransaction.proceed(joinPoint);
+//
+//        } catch (InterruptedException e) {
+//            // TODO: replace error statement
+//            log.error("DistributedLock lock interrupted");
+//            throw new InterruptedException(e.getMessage());
+//        } finally {
+//            if (rLock.isLocked() && rLock.isHeldByCurrentThread()) {
+//                rLock.unlock();
+//            }
+//        }
 
-            if (!available) {
-                return false;
-            }
-            return aopForTransaction.proceed(joinPoint);
+        // 예외 처리 및 락 획득을 재시도하는 로직
+        int retryCount = 0;
 
-        } catch (InterruptedException e) {
-            // TODO: replace error statement
-            log.error("DistributedLock lock interrupted");
-            throw new InterruptedException(e.getMessage());
-        } finally {
-            if (rLock.isLocked() && rLock.isHeldByCurrentThread()) {
-                rLock.unlock();
+        while (retryCount < MAX_RETRY_COUNT) {
+            try {
+                boolean available = rLock.tryLock(distributedLock.waitTime(),
+                    distributedLock.leaseTime(), distributedLock.timeUnit());
+
+                if (available) {
+                    return aopForTransaction.proceed(joinPoint);
+                } else {
+                    retryCount++;
+                    Thread.sleep(RETRY_DELAY);
+                }
+            } catch (InterruptedException e) {
+                log.error("DistributedLock lock interrupted");
+                throw new InterruptedException(e.getMessage());
+            } finally {
+                if (rLock.isLocked() && rLock.isHeldByCurrentThread()) {
+                    // Todo: flushAll 은 현재 test 환경에서만 사용
+                    flushAll();
+                    rLock.unlock();
+                }
             }
         }
+
+        throw new IllegalStateException("DistributedLock lock failed after retries");
     }
 }
-
